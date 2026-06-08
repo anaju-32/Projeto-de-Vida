fe15b5a5057d48311ea93710660d00606eb3b015
anaju-32 <ana.miretzki@escola.pr.gov.br>
2026-06-08 08:17:00 -0300

    ???

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 
+
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-05-31T00:00:00");
 const tempoObjetivo2 = new Date("2026-11-01T00:00:00");
@@ -21,6 +22,7 @@ const tempoObjetivo3 = new Date("2026-11-08T00:00:00");
 const tempoObjetivo4 = new Date("2026-11-24T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
 
 
 function calculaTempo(tempoObjetivo) {
@@ -54,6 +56,3 @@ function comecaCronometro() {
     atualizaCronometro();
     setInterval(atualizaCronometro, 1000);
 }
-
-comecaCronometro();
-//000
